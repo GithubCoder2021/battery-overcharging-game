a7d9c2a5adce3bd14360bed9ef55f49d42852960
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-13 12:32:53 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
 percent = new Decimal(0);
+charge = new Decimal(1);
+setInterval(function() {
+  percent = percent.add(charge);
+	
+  document.getElementById("percent").innerText = percent;
+}, 1000);
