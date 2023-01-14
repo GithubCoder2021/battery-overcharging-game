46952cd635a5ac6788b3172275634c27fb53aadc
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-13 20:54:14 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,18 @@
 let percent = new Decimal(0);
 let charge = new Decimal(1);
+let upg1 = new Decimal(20);
 setInterval(function() {
   percent = percent.add(charge);
 	
   document.getElementById("percent").innerText = percent;
 }, 1000);
+
+function buyUpg1() { 
+   costCheck = percent.greaterThanOrEqualTo(upg1);
+   if (costCheck == true) {
+	   percent = percent.sub(upg1);
+	   charge = charge.mul(2);
+	   upg1 = upg1.mul(2);      
+   } 
+	
+}
