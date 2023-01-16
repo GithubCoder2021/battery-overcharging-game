3473d4f46787bf1e911f269ca0b986a89bb418ca
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-15 21:33:32 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 let percent = new Decimal(0);
 let charge = new Decimal(1);
-let upg1 = new Decimal(20);
+let upg1 = new Decimal(10);
+let upg2 = new Decimal(250);
+let upg3 = new Decimal(1500);
+
 setInterval(function() {
   percent = percent.add(charge);
 	
@@ -12,9 +15,31 @@ function buyUpg1() {
    if (costCheck == true) {
 	   percent = percent.sub(upg1);
 	   document.getElementById("percent").innerText = percent;
-	   charge = charge.mul(2);
+	   charge = charge.add(1);
 	   upg1 = upg1.mul(2);  
 	   document.getElementById("upg1").innerText = upg1;
+   } 
+	
+}
+function buyUpg2() { 
+   costCheck = percent.greaterThanOrEqualTo(upg2);
+   if (costCheck == true) {
+	   percent = percent.sub(upg2);
+	   document.getElementById("percent").innerText = percent;
+	   charge = charge.add(10);
+	   upg2 = upg2.mul(2);  
+	   document.getElementById("upg2").innerText = upg2;
+   } 
+	
+}
+function buyUpg2() { 
+   costCheck = percent.greaterThanOrEqualTo(upg3);
+   if (costCheck == true) {
+	   percent = percent.sub(upg3);
+	   document.getElementById("percent").innerText = percent;
+	   charge = charge.add(100);
+	   upg3 = upg3.mul(2);  
+	   document.getElementById("upg3").innerText = upg3;
    } 
 	
 }
