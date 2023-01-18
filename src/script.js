b3cccd6224e61f11c52c9795e83da1557351fdf8
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-18 18:34:58 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,6 @@ let upg4 = new Decimal(5000);
 
 setInterval(function() {
   percent = percent.add(charge);
-  save();
   document.getElementById("percent").innerText = percent;
 }, 1000);
 
@@ -60,37 +59,4 @@ function buyUpg4() {
 	   document.getElementById("upg4").innerText = upg4;
    } 
 	
-}
-function save() {
-  var save = {
-    percent: percent,
-    charge: charge,
-    upg1: upg1,
-    upg2: upg2,
-    upg3: upg3,
-    upg4: upg4
-  }
- localStorage.setItem("save",JSON.stringify(save));
- var savegame = JSON.parse(localStorage.getItem("save"));
- if (typeof savegame.percent !== "undefined") percent = savegame.percent;
- if (typeof savegame.charge !== "undefined") charge = savegame.charge;
- if (typeof savegame.upg1 !== "undefined") upg1 = savegame.upg1;
- if (typeof savegame.upg2 !== "undefined") upg2 = savegame.upg2;
- if (typeof savegame.upg3 !== "undefined") upg3 = savegame.upg3;
- if (typeof savegame.upg4 !== "undefined") upg4 = savegame.upg4;
-}
-
-function hardReset() {
- reset = "ARE YOU SURE?!!!";
- if (comfirm(reset) == true) {
-  alert("NOOOOOOOO...")
-  localStorage.removeItem("save")
-  let percent = new Decimal(0);
-  let charge = new Decimal(1);
-  let upg1 = new Decimal(10);
-  let upg2 = new Decimal(250);
-  let upg3 = new Decimal(2500);
-  let upg4 = new Decimal(5000);
-  window.location.reload();
- }
 }
