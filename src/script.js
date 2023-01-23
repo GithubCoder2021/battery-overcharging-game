144ce625ea96a04aa38c90fe61fffe1930591249
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-22 22:58:50 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,63 +1,54 @@
-let percent = normal(-Infinity);
-let charge = normal(0);
-let upg1 = normal(1);
-let upg2 = normal(2.39794);
-let upg3 = normal(3.39794);
-let upg4 = normal(3.69897);
+let percent = new Decimal(0);
+let charge = new Decimal(1);
+let upg1 = new Decimal(10);
+let upg2 = new Decimal(250);
+let upg3 = new Decimal(2500);
+let upg4 = new Decimal(5000);
 
 setInterval(function() {
-  percent = addLogs(percent,charge);
-  percent = normal(percent);
+  percent = percent.add(charge);
   document.getElementById("percent").innerText = percent;
 }, 1000);
 
 function buyUpg1() { 
-   if (percent >= upg1) {
-	   percent = subtractLogs(percent,upg1);
-           percent = normal(percent);
+   if (percent.gte(upg1)) {
+	   percent = percent.sub(upg1);
 	   document.getElementById("percent").innerText = percent;
-	   charge = addLogs(charge,0);
-	   upg1 = multiplyLogs(upg1,0.30103);
-           upg1 = normal(upg1); 
+	   charge = charge.add(1);
+	   upg1 = upg1.mul(2);
 	   document.getElementById("upg1").innerText = upg1;
    } 
 	
 }
 
 function buyUpg2() { 
-   if (percent >= upg2) {
-	   percent = subtractLogs(percent,upg2);
-           percent = normal(percent);
+   if (percent.gte(upg2)) {
+	   percent = percent.sub(upg2);
 	   document.getElementById("percent").innerText = percent;
-	   charge = addLogs(charge,1);
-	   upg2 = multiplyLogs(upg,0.20411);
-           upg2 = normal(upg2); 
+	   charge = charge.add(10);
+	   upg2 = upg2.mul(1.6);
 	   document.getElementById("upg2").innerText = upg2;
    } 
 	
 }
 
-function buyUpg3() { 
-   if (percent >= upg3) {
-	   percent = subtractLogs(percent,upg3);
-           percent = normal(percent);
+function buyUpg4() { 
+   if (percent.gte(upg3)) {
+	   percent = percent.sub(upg3);
 	   document.getElementById("percent").innerText = percent;
-	   charge = addLogs(charge,2);
-	   upg3 = multiplyLogs(upg,0.1139);
-           upg3 = normal(upg3); 
+	   charge = charge.add(100);
+	   upg3 = upg3.mul(1.3);
 	   document.getElementById("upg3").innerText = upg3;
    } 
 	
 }
 
 function buyUpg4() { 
-   if (percent >= upg4) {
-	   percent = subtractLogs(percent,upg4);
-           percent = normal(percent);
+   if (percent.gte(upg4)) {
+	   percent = percent.sub(upg4);
 	   document.getElementById("percent").innerText = percent;
-	   charge = multiplyLogs(charge,0.30103);
-	   upg4 = multiplyLogs(upg,0.342422);
-           upg4 = normal(upg4); 
+	   charge = charge.mul(2);
+	   upg4 = upg4.mul(1.9);
 	   document.getElementById("upg4").innerText = upg4;
    } 
 	
