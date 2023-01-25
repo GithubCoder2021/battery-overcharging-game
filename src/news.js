a3759729467287a33d757bd791c12da4d235aa95
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-25 11:25:23 -0500

    Create news.js

---
+++ src/news.js
@@ -0,0 +1,31 @@
+setInterval (function() {
+  const news = [
+  "Insert news here", 
+  "This is a game about overcharging a battery",
+  "Wait, didn't NO! do battery overcharging to absolute infinity?",
+  "I have two sides: My battery is empty...  My battery is full!!!",
+  "It seems like there is a monster on the lose! It was made by a battery",
+  "BREAKING NEWS!!! NewFall just made the button reach Too much massivefinity clicks.",
+  "When will Battery overcharging to NEVER come out?",
+  "How many prestige layers will there be",
+  "I am so bored", 
+  "Hevipelle finally released the reality update after 4 years! (+5 hours) ",
+  "Mathis R.V. brought me back to the world of googology. Credit to him and everyone else",
+  "Hmmm... It seems like we are stuck in a softcap",
+  "Breaking news!!! Aarex Tiaokhiao is planning on ending NG+3 soon... ",
+  "FREE LUNCH!!!",
+  "Too be honest, SuperSpruce is the best incremental game youtuber ever!!!",
+  "This game is running on break_infinity.js by Patashu.",
+  "Oops, the news ticker was just a joke after all... ", 
+  "News ticker: STOP THE BATTERY JOKES PIXELVERSE/ GC2021 !! Me: No get rekt news ticker. Don't mess with Pixelverse (me)",
+  "Credit goes to NO!, Newfall 2023, Mathis, WTIF2023, Superspruce, DShalmin, Hevipelle  and everyone else (including you the player) for inspiring me to create this ",
+  "Breaking news, there is a protest going on outside the battery facility!",
+  "There is no 9th dimension in Antimatter Dimensions because -- [CONNECTION LOST]",
+  "Read this sentence in a mirror: !diputs ma I !AHAH",
+  "Batteries go all the way back to 1780. Figure out the rest of the history of batteries yourself!",
+  "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",];
+
+document.getElementById("news").innerText = news[Math.floor(Math.random()*news.length)];
+}, 10000);
+
+  
